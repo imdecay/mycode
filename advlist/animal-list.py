c7881e530671626079d63b6c4b00d560cc47d751
Dana Kaempen <imdecay@users.noreply.github.com>
2021-10-04 19:26:43 +0000

    Lab 12

---
+++ advlist/animal-list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   List - 3-letter animal list"""
+
+def main():
+    # create a list called list1
+    anim_list1 = [ "Fox", "Fly", "Ant", "Bee", "Cod", "Cat", "Dog", "Yak", "Cow", "Hen", "Koi", "Hog", "Jay", "Kit" ]
+
+    # display list1
+    print(anim_list1)
+
+
+main()
+
